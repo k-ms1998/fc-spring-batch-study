9244aebbe142838650451786ba89c8022a3f6002
Min Seop Kim <k_ms1998@naver.com>
2022-07-25 22:27:04 +0900

    Project Setup

---
+++ Part1/src/main/java/com/points/PointsManagementProject/PointsManagementProjectApplication.java
@@ -0,0 +1,13 @@
+package com.points.PointsManagementProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PointsManagementProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PointsManagementProjectApplication.class, args);
+	}
+
+}
---
+++ Part1/src/test/java/com/points/PointsManagementProject/PointsManagementProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.points.PointsManagementProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PointsManagementProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
