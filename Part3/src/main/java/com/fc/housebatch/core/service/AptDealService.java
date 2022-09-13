f432ae354708917a47d9673ddb98abf2f43e885c
Min Seop Kim <k_ms1998@naver.com>
2022-09-13 17:45:27 +0900

    Add #4: Item Processor 에 필요한 로직 구현

--- Part3/src/main/java/com/fc/housebatch/core/service/AptDealService.java
+++ Part3/src/main/java/com/fc/housebatch/core/service/AptDealService.java
@@ -1,12 +1,17 @@
 package com.fc.housebatch.core.service;
 
 import com.fc.housebatch.core.dto.AptDealDto;
+import com.fc.housebatch.core.dto.AptDto;
 import com.fc.housebatch.core.entity.Apt;
 import com.fc.housebatch.core.entity.AptDeal;
 import com.fc.housebatch.core.repository.AptDealRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -36,5 +41,25 @@ public class AptDealService {
         return aptDealRepository.findAptDealByAptAndExclusiveAreaAndDealDateAndDealAmountAndFloor(
                 apt, dto.getArea(), dto.dealDateFromAptDealDto(), dto.dealAmountFromAptDealDto(), dto.getFloor())
                 .orElseGet(() -> AptDeal.of(apt, dto));
+    }
+
+    /**
+     * guLawdCd 랑 dealDate 파라미터 값들이 일치하는 AptDeal 데이터들 가져오기
+     */
+    public List<AptDeal> findGuLawdCdAndDealDate(String guLawdCd, LocalDate dealDate) {
+        return aptDealRepository.findByDealCanceledIsFalseAndDealDateEquals(dealDate)
+                .stream()
+                .filter(aptDeal -> aptDeal.getApt().getGuLawdCd().equals(guLawdCd))
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * AtpDeal -> AptDto 로 변환해서 반환하기
+     */
+    public List<AptDto> guLawdCdAndDealDateToAtpDto(String guLawdCd, LocalDate dealDate){
+        return findGuLawdCdAndDealDate(guLawdCd, dealDate)
+                .stream()
+                .map(aptDeal -> new AptDto(aptDeal.getApt().getAptName(), aptDeal.getDealAmount()))
+                .collect(Collectors.toList());
     }
 }
