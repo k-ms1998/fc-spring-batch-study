d3eb27c00bed6c99a9fce3dc28480c1b5d1357a2
Min Seop Kim <k_ms1998@naver.com>
2022-10-26 17:53:06 +0900

    Add #7: Custom Exception for annotation exceptions (@Valid)

--- Part4/calendar-api/src/main/java/com/fc/project/api/exception/GlobalExceptionHandler.java
+++ Part4/calendar-api/src/main/java/com/fc/project/api/exception/GlobalExceptionHandler.java
@@ -5,16 +5,24 @@ import com.fc.project.core.exception.ErrorCode;
 import lombok.Data;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.Optional;
 
 /**
  * Exception Handler:
  * Exception 이 발생했을때, exception 을 던지지 않고, Response 로  에러코드를 반환하는 방식으로 처리
  * Because, exception 을 던지는 방식은 overhead 가 많기 때문에
+ *
+ * @ControllerAdvice:
+ * Specialization of @Component for classes that declare
+ * @ExceptionHandler, @InitBinder, or @ModelAttribute methods to be shared across multiple @Controller classes.
  */
-//@ControllerAdvice
+@ControllerAdvice
 public class GlobalExceptionHandler {
 
     /**
@@ -28,6 +36,24 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(new ErrorResponse(errorCode, errorCode.getMessage()),
                 ErrorCodeHttpStatusMapper.mapToStatus(errorCode));
     }
+
+    /**
+     * MethodArgumentNotValidException: Exception to be thrown when validation on an argument annotated with @Valid fails
+     * 도메인에 대해서, @Valid 로 검증할 파라미터들 중에서 exception 이 발생했을때 catch 해서 오류 처리
+     */
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidationAnnotationException(MethodArgumentNotValidException ex) {
+        final ErrorCode errorCode = ErrorCode.VALIDATION_FAIL;
+
+        return new ResponseEntity<>(new ErrorResponse(
+                        errorCode,
+                        Optional.ofNullable(ex.getBindingResult().getFieldError())
+                                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                                .orElse(errorCode.getMessage())),
+                ErrorCodeHttpStatusMapper.mapToStatus(errorCode));
+    }
+    
+
 
     @Getter
     @RequiredArgsConstructor
